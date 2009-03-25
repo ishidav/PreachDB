dc1afa82cc0059f679e8ecf175890ff5e06f938c
binghamb <binghamb>
2009-03-25 23:54:44 +0000

    Fixed the deadlock bug; now all termination signals come from the root only. Cleaned up some code and moved the default model to tiny.erl.

--- DEV/SRC/preach.erl
+++ DEV/SRC/preach.erl
@@ -25,15 +25,14 @@
 % along with this program; if not, write to the Free Software
 % Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 %--------------------------------------------------------------------------------
--module(preach_term).
--compile(inline). %inlines all functions of 24 lines or less
+-module(preach).
+% -compile(inline). %inlines all functions of 24 lines or less
 -export([start/3,startWorker/1]).
 
+%% configuration-type functions
 timeoutTime() -> 3000.
 
-rootPID(Names) -> hd(Names).
-
-mynode(Index) -> brad@marmot.cs.ubc.ca;
+mynode(_Index) -> brad@marmot.cs.ubc.ca;
 mynode(Index) -> 
 	lists:nth(1+((Index-1) rem 2), [
 			brad@marmot.cs.ubc.ca,
@@ -44,23 +43,19 @@ mynode(Index) ->
 % other machines: 
 % brad@fossa.cs.ubc.ca, brad@indri.cs.ubc.ca
 
-waitForTerm([], TotalStates) ->	
-	TotalStates;
-waitForTerm([FirstPID | RestPID], TotalStates) ->
-	receive
-		{FirstPID, NumStates, done} -> 
-		io:format("PID ~w: worker thread ~w has reported termination~n", [self(), FirstPID])
-	end,
-	waitForTerm(RestPID, TotalStates + NumStates).
+compressState(State) ->
+	stress:stateToInt(State).
+%	stress:stateToBits(State).
+%	State.
 
-purgeRecQ(Count) ->
-	receive
-		_Garbage ->
-			purgeRecQ(Count+1)
-	after
-		0 ->
-			Count
-	end.
+decompressState(CompressedState) ->
+		stress:intToState(CompressedState).
+%		stress:bitsToState(CompressedState).
+%		CompressedState.
+
+%% end of configuration-type functions
+
+rootPID(Names) -> hd(Names).
 
 %%----------------------------------------------------------------------
 %% Function: start/3
@@ -74,7 +69,8 @@ purgeRecQ(Count) ->
 %%----------------------------------------------------------------------
 start(Start,End,P) ->
  	T0 = now(),
-	Names = initThreads([], P,End), 
+	Names = initThreads([], P,End),
+	
 	sendStates(Start, Names),
 	NumSent = length(Start),
 	reach([], End, Names,dict:new(),{NumSent,0}), % if this returns, End was not found
@@ -82,26 +78,33 @@ start(Start,End,P) ->
 	io:format("PID ~w: waiting for workers to report termination...~n", [self()]),
 	NumStates = waitForTerm(Names, 0),
   	Dur = timer:now_diff(now(), T0)*1.0e-6,
-	NumMessages = purgeRecQ(0),
+	NumPurged = purgeRecQ(0),
 
 	io:format("----------~n"),
 	io:format("REPORT:~n"),
 	io:format("\tTotal of ~w states visited~n", [NumStates]),
-	io:format("\t~w messages purged from the receive queue~n", [NumMessages]),
+	io:format("\t~w messages purged from the receive queue~n", [NumPurged]),
 	io:format("\tExecution time: ~f seconds~n", [Dur]),
 	io:format("\tStates visited per second: ~w~n", [trunc(NumStates/Dur)]),
 	io:format("----------~n"),
 	done.	
 
-compressState(State) ->
-	stress:stateToInt(State).
-%	stress:stateToBits(State).
-%	State.
 
-decompressState(CompressedState) ->
-		stress:intToState(CompressedState).
-%		stress:bitsToState(CompressedState).
-%		CompressedState.
+waitForTerm(PIDs, _) ->
+	F = fun(_PID, Total) -> 	receive {DiffPID, NumStates, done} -> 
+				io:format("PID ~w: worker thread ~w has reported termination~n", [self(), DiffPID])
+				end,
+				Total + NumStates end,
+	lists:foldl(F, 0, PIDs).
+
+purgeRecQ(Count) ->
+	receive
+		_Garbage ->
+			purgeRecQ(Count+1)
+	after
+		0 ->
+			Count
+	end.
 
 %%----------------------------------------------------------------------
 %% Function: sendStates/2
@@ -117,30 +120,9 @@ sendStates([], _Names) ->
 	ok;
 
 sendStates([First | Rest], Names) ->
-	Owner = lists:nth(stateHash(First,length(Names)), Names),
+	Owner = lists:nth(1+erlang:phash2(First,length(Names)),Names),
 	Owner ! {compressState(First), state},
 	sendStates(Rest, Names).
-
-%%----------------------------------------------------------------------
-%% Function: stateHash/2
-%% Purpose : Maps states to owning threads. 
-%% 
-%% Args    : State is the state find the owner of
-%%			 (assumes states are tuples)
-%%			 
-%% Returns : Owning thread in the range [1..P]
-%%     
-%%----------------------------------------------------------------------
-stateHash(State, P) ->
-% a silly has function
-%	Hash = lists:sum(tuple_to_list(First)),
-% a bit less silly
-%	Hash = 1*element(1,State)+2*element(2,State)+3*element(3,State)+4*element(4,State)
-%		+5*element(5,State)+6*element(6,State)+7*element(7,State),
-% probably sufficient
-	Hash = erlang:phash2(State, P),
-	1+(Hash rem P).
-
 
 %%----------------------------------------------------------------------
 %% Function: initThreads/3
@@ -156,8 +138,7 @@ initThreads(Names, 1, _Data) ->
 	[self() | Names];
 % Data is just End right now
 initThreads(Names, NumThreads, Data) ->
-	% ID = spawn(preach, startWorker, [Data]),
-	ID = spawn(mynode(NumThreads),preach_term,startWorker,[Data]),
+	ID = spawn(mynode(NumThreads),preach,startWorker,[Data]),
 	io:format("Starting worker thread on ~w with PID ~w~n", [mynode(NumThreads), ID]),
 	FullNames = initThreads([ID | Names], NumThreads-1, Data),
 	ID ! FullNames. % send each worker the PID list
@@ -204,71 +185,31 @@ reach([FirstState | RestStates], End, Names, BigList, {NumSent, NumRecd}) ->
 	true ->
 		CurState = decompressState(FirstState),
 		NewStates = stress:transition(CurState, start),
-		EndFound = stateMatch(CurState,End),
+		EndFound = stress:stateMatch(CurState,End),
 
 		if EndFound ->
 			io:format("=== End state ~w found by PID ~w ===~n", [End,self()]),
-			terminateAll(lists:delete(self(),Names)), % terminate all other processes
-			terminateMe(dict:size(BigList), rootPID(Names));
-		true ->
-			sendStates(NewStates, Names),
-			NewNumSent = NumSent + length(NewStates),
-%			reach(RestStates, End, Names, dict:append(FirstState, true, BigList)) % grow the big list
-			reach(RestStates, End, Names, dict:append(FirstState, true, BigList), {NewNumSent, NumRecd}) % grow the big list
-		end
+			rootPID(Names) ! end_found;
+		   true -> do_nothing
+		end,
+		sendStates(NewStates, Names),
+		NewNumSent = NumSent + length(NewStates),
+		reach(RestStates, End, Names, dict:append(FirstState, true, BigList), {NewNumSent, NumRecd}) % grow the big list
 	end;
 
 % StateQ is empty, so check for messages. If none are found, we die
 reach([], End, Names, BigList, {NumSent, NumRecd}) ->
-	Ret = checkMessageQ(timeout, dict:size(BigList), Names, {NumSent, NumRecd}, 0),
+	Ret = checkMessageQ(timeout, dict:size(BigList), Names, {NumSent, NumRecd}, 0, []),
 	if Ret == done ->
 			done;
 	   true ->
 		{NewQ, NewNumRecd} = Ret,
-		NewQ2 = lists:usort(NewQ), % remove any duplicate states
-		reach(NewQ2, End, Names, BigList, {NumSent, NewNumRecd})
+%		NewQ2 = lists:usort(NewQ), % remove any duplicate states
+		reach(NewQ, End, Names, BigList, {NumSent, NewNumRecd})
 	end.
-
-pollWorkers([], _) ->
-	{0,0};
-
-% deadlock bug here - what if and end state is found by a worker 
-% while the root is waiting for a poll message?
-pollWorkers([ThisPID | Rest], {RootSent, RootRecd}) ->
-	if ThisPID == self() ->
-		io:format("PID ~w: polling myself~n", [self()]),
-		no_need_to_send;
-	true ->
-		io:format("PID ~w: sending pause signal to PID ~w~n", [self(),ThisPID]),
-		ThisPID ! pause
-	end,
-
-	{S, R} = pollWorkers(Rest, {RootSent, RootRecd}),
-
-	if ThisPID == self() ->
-		io:format("PID ~w: root CommAcc is {~w,~w}~n", [self(),RootSent,RootRecd]),
-		{S+RootSent, R+RootRecd};
-	true ->
-		receive
-		{ThisSent, ThisRecd, poll} ->
-			io:format("PID ~w: Got a worker CommAcc of {~w,~w}~n", [self(),ThisSent,ThisRecd]),
-			{S+ThisSent, R+ThisRecd}
-		end
-	end.
-
-resumeWorkers([]) ->
-	ok;
-
-resumeWorkers([ThisPID | Rest]) ->
-	if ThisPID == self() ->
-		no_need_to_send;
-	true ->
-		ThisPID ! resume
-	end,
-	resumeWorkers(Rest).
 
 %%----------------------------------------------------------------------
-%% Function: checkMessageQ/4
+%% Function: checkMessageQ/5-6
 %% Purpose : Polls for incoming messages
 %%
 %% Args    : timeout is atomic indicator that we are polling;
@@ -277,81 +218,85 @@ resumeWorkers([ThisPID | Rest]) ->
 %%		Names is the list of PIDs
 %%	     {NumSent, NumRecd} is the running total of the number of states
 %%	     sent and received, respectively
+%%	     NewStates is a list accumulating the new received states
+%%	     Depth is just for debugging and testing purposes
 %% Returns : EITHER List of received states in the form of
 %%		{NewStateQ, NewNumRecd} where NewNumRecd is
 %%		equal to NumRecd + length(NewStateQ)
 %%		OR the atom 'done' to indicate we are finished visiting states
 %%     
 %%----------------------------------------------------------------------
-checkMessageQ(timeout, BigListSize, Names, {NumSent, NumRecd}, _) when self() == hd(Names) ->
-%	io:format("PID ~w (root): checking my message queue; ~w messages received so far~n", [self(), NumRecd]), % for debugging
+checkMessageQ(timeout, BigListSize, Names, {NumSent, NumRecd}, _, NewStates) ->
+%	io:format("PID ~w: checking my message queue; ~w messages received so far~n", [self(), NumRecd]), % for debugging
+	Timeout = if hd(Names) == self() -> timeoutTime(); true -> infinity end,
 	receive
 	{State, state} ->
-%		io:format("PID ~w received state ~w~n", [self(), State]), % for debugging
-		case checkMessageQ(notimeout,BigListSize,Names,{NumSent, NumRecd+1},0) of
-			{NewStateQ,NewNumRecd} -> {[State | NewStateQ], NewNumRecd};
-			done -> done
-		end;
-	die -> 
-		terminateMe(BigListSize, rootPID(Names))
-	after 
-		timeoutTime() -> % wait for timeoutTime() ms   
-		io:format("PID ~w: Root has timed out, polling workers now...~n", [self()]),
-		CommAcc = pollWorkers(Names, {NumSent, NumRecd}),
-		CheckSum = element(1,CommAcc) - element(2,CommAcc),
-		if CheckSum == 0 ->
-			% time to die
-			io:format("=== No End states were found ===~n"),
-			terminateAll(tl(Names)),
-			terminateMe(BigListSize, rootPID(Names));
-		true ->
-			% resume other processes and wait again for new states or timeout
-			io:format("PID ~w: unusual, checksum is ~w, will wait again for timeout...~n", [self(),CheckSum]),
-			resumeWorkers(Names),
-			checkMessageQ(timeout,BigListSize,Names,{NumSent,NumRecd},0)
-		end
-	end;
-
-checkMessageQ(timeout, BigListSize, Names, {NumSent, NumRecd}, _) ->
-%	io:format("PID ~w (worker): checking my message queue; ~w messages received so far~n", [self(), NumRecd]), % for debugging
-	receive
-	{State, state} ->
-%		io:format("PID ~w received state ~w~n", [self(), State]), % for debugging
-		case checkMessageQ(notimeout,BigListSize,Names,{NumSent, NumRecd+1},0) of 
-		{NewStateQ, NewNumRecd} -> {[State | NewStateQ], NewNumRecd};
-		done -> done
-		end;
+		checkMessageQ(notimeout,BigListSize,Names,{NumSent, NumRecd+1},0, [State|NewStates]);
 	pause -> % report # messages sent/received
 		rootPID(Names) ! {NumSent, NumRecd, poll},
 		receive
 		resume ->
-			checkMessageQ(timeout, BigListSize, Names, {NumSent, NumRecd},0);
+			checkMessageQ(timeout, BigListSize, Names, {NumSent, NumRecd},0, NewStates);
 		die ->
 			terminateMe(BigListSize, rootPID(Names))
 		end;
 	die -> 
+		terminateMe(BigListSize, rootPID(Names));
+	end_found -> 
+		terminateAll(tl(Names)),
 		terminateMe(BigListSize, rootPID(Names))
+	after Timeout -> % wait for timeoutTime() ms if root 
+		io:format("PID ~w: Root has timed out, polling workers now...~n", [self()]),
+		CommAcc = pollWorkers(Names, {NumSent, NumRecd}),
+		CheckSum = element(1,CommAcc) - element(2,CommAcc),
+		if CheckSum == 0 ->	% time to die
+			io:format("=== No End states were found ===~n"),
+			terminateAll(tl(Names)),
+			terminateMe(BigListSize, rootPID(Names));
+		true ->	% resume other processes and wait again for new states or timeout
+			io:format("PID ~w: unusual, checksum is ~w, will wait again for timeout...~n", [self(),CheckSum]),
+			resumeWorkers(tl(Names)),
+			checkMessageQ(timeout,BigListSize,Names,{NumSent,NumRecd},0, NewStates)
+		end
 	end;
 
-%checkMessageQ(notimeout, BigListSize, Names, {NumSent, NumRecd}, Depth) when Depth == 1000->
-%	{[], NumRecd};
+%checkMessageQ(notimeout, BigListSize, Names, {NumSent, NumRecd}, Depth, NewStates) when Depth == 1000->
+%	{NewStates, NumRecd};
 
 % Get all queued messages without waiting
-checkMessageQ(notimeout, BigListSize, Names, {NumSent, NumRecd}, Depth) ->
+checkMessageQ(notimeout, BigListSize, Names, {NumSent, NumRecd}, Depth, NewStates) ->
 	receive
 	% could check for pause messages here
         {State, state} ->
-%		io:format("PID ~w received state ~w~n", [self(), State]), % for debugging
-		case checkMessageQ(notimeout,BigListSize,Names,{NumSent, NumRecd+1}, Depth+1) of
-		{NewStateQ,NewNumRecd} -> {[State | NewStateQ], NewNumRecd};
-		done -> done
-		end;
+		checkMessageQ(notimeout,BigListSize,Names,{NumSent, NumRecd+1}, Depth+1, [State|NewStates]);
 	die -> 
 		terminateMe(BigListSize, rootPID(Names))
 	after 
-		1 -> % wait for 0 ms   
-			{[], NumRecd} 
+		0 -> % wait for 0 ms  
+			{NewStates, NumRecd} 
 	end.
+
+pollWorkers([], _) ->
+	{0,0};
+
+pollWorkers([ThisPID | Rest], {RootSent, RootRecd}) when ThisPID == self() ->
+	{S, R} = pollWorkers(Rest, {RootSent, RootRecd}),
+	io:format("PID ~w: root CommAcc is {~w,~w}~n", [self(),RootSent,RootRecd]),
+	{S+RootSent, R+RootRecd};
+
+pollWorkers([ThisPID | Rest], {RootSent, RootRecd}) ->
+	io:format("PID ~w: sending pause signal to PID ~w~n", [self(),ThisPID]),
+	ThisPID ! pause,
+	{S, R} = pollWorkers(Rest, {RootSent, RootRecd}),
+	receive
+	{ThisSent, ThisRecd, poll} ->
+		io:format("PID ~w: Got a worker CommAcc of {~w,~w}~n", [self(),ThisSent,ThisRecd]),
+		{S+ThisSent, R+ThisRecd}
+	end.
+
+resumeWorkers(PIDs) ->
+	lists:map(fun(PID) -> PID ! resume end, PIDs),
+	ok.
 
 terminateMe(NumStatesVisited, RootPID) ->
 	io:format("PID ~w: was told to die; visited ~w unique states~n", [self(), NumStatesVisited]),
@@ -365,59 +310,18 @@ terminateMe(NumStatesVisited, RootPID) ->
 %% Returns :
 %%     
 %%----------------------------------------------------------------------
-terminateAll([]) ->	
-	done;
-terminateAll([FirstPID | RestPID]) ->
-	FirstPID ! die,
-	terminateAll(RestPID).
-
-%% the following functions to be removed. For "simple" testing only.
-%% currently, transition, guard and action should all be implemented
-%% in gospel. stateMatch should be implemented elsewhere, but we're
-%% not sure where it should go at the moment.
-
-%% Default transition unless we're MC-ing someting specific
-transition(State, start) ->
-	T = transition(2, State), % 2 is the number of guarded commands
-	[X || X <- T, X /= null];
-transition(0, _) ->
-	[];
-transition(Index, State) ->
-	[guard(Index, State) | transition(Index-1, State)].
-	
-%% Default guard
-guard(Index, State) ->
-	case Index of
-		1 -> 	if 
-				State == {0,0} -> action(Index, State);
-				true -> null
-			end;
-		2 ->	if 
-				((State == {1,0}) or (State == {0,1})) -> action(Index, State);
-				true -> null
-			end
-	end.
-
-% action may be a function of State, but not for the simple example
-action(Index, State) ->
-	case Index of
-		1 -> 	{0,1};
-		2 ->	{0,0}
-	end.
-
-% may need to allow End to be a list of states, possibly
-% with don't care variables
-stateMatch(State, End) ->
-	Pairs = lists:zip(tuple_to_list(State), tuple_to_list(End)),
-	Eq = fun({X,Y}) -> (X == Y) or (Y == dc) end,
-	lists:all(Eq, Pairs).
-
+terminateAll(PIDs) ->
+	lists:map(fun(PID) -> PID ! die end, PIDs),
+	done.
 
 %--------------------------------------------------------------------------------
 %                             Revision History
 %
 %
 % $Log: preach.erl,v $
+% Revision 1.9  2009/03/25 23:54:44  binghamb
+% Fixed the deadlock bug; now all termination signals come from the root only. Cleaned up some code and moved the default model to tiny.erl.
+%
 % Revision 1.8  2009/03/22 23:52:50  binghamb
 % Substantial changes to communication. Implemented a termination scheme similar to Stern and Dill's. This has introduced a rarely occuring deadlock bug which is easily fixable. Also more verbose output.
 %
